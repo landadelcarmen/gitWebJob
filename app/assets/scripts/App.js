982e9a707d58817ffc1a1114dab6480d42efb350
aMedinaCh <a.medina@outlook.com>
2018-04-10 13:51:45 -0500

    modal completed

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,9 +1,11 @@
 import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 import RevealOnScroll from './modules/RevealOnScroll';
 
 var mobileMenu = new MobileMenu();
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
