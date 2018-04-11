524a46431933ea3071e2b3de75cd246c354612d7
aMedinaCh <a.medina@outlook.com>
2018-04-11 00:15:07 -0500

    browser support completed

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var svgSprite = require('gulp-svg-sprite');
 var rename= require('gulp-rename');
 var del= require('del');
+var svg2png = require('gulp-svg2png');
 
 var config = {
   mode: {
@@ -19,15 +20,21 @@ gulp.task('beginClean', function() {
 	return del(['./app/temp/sprite', './app/assets/images/sprites']);
 });
 
-gulp.task('createSprite', ['beginClean'],function() {
+gulp.task('createSprite',['beginClean'] ,function() {
 	return gulp.src('./app/assets/images/icons/**/*.svg')
 	.pipe(svgSprite(config))
 	.pipe(gulp.dest('./app/temp/sprite/'));
 });
 
-gulp.task('copySpriteGraphic', ['createSprite'],function() {
-	return gulp.src('./app/temp/sprite/css/**/*.svg')
-	.pipe(gulp.dest('./app/assets/images/sprites/'));
+gulp.task('createPngCopy', ['createSprite'], function() {
+  	return gulp.src('./app/temp/sprite/css/*.svg')
+    .pipe(svg2png())
+    .pipe(gulp.dest('./app/temp/sprite/css'));
+});
+
+gulp.task('copySpriteGraphic', ['createPngCopy'], function() {
+  	return gulp.src('./app/temp/sprite/css/**/*.{svg,png}')
+    .pipe(gulp.dest('./app/assets/images/sprites'));
 });
 
 gulp.task('copySpriteCss', ['createSprite'],function() {
@@ -40,4 +47,4 @@ gulp.task('endClean', ['copySpriteGraphic','copySpriteCss'],function() {
 	return del(['./app/temp/sprite']);
 });
 
-gulp.task('icons', ['beginClean', 'createSprite', 'copySpriteGraphic','copySpriteCss', 'endClean']);
+gulp.task('icons', ['beginClean', 'createSprite', 'createPngCopy', 'copySpriteGraphic', 'copySpriteCss', 'endClean']);
